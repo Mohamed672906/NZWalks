bd12b7ed0b2c3fdac7a10e198f66c0cc796434dd
Mohamed Ali <mohamedalibk66@gmail.com>
2025-08-04 00:23:31 +0300

    Creating the Dbcontext Class

---
+++ NZWalks.API/Data/NZWalkDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalkDbContext:DbContext
+    {
+
+        public NZWalkDbContext(DbContextOptions dbContextOptions ):base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+       // public DbSet<Image> Images { get; set; }
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
