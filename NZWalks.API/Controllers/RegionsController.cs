50d6c435a44bd2d20c95525f582dff351badb793
Mohamed Ali <mohamedalibk66@gmail.com>
2025-08-10 23:34:25 +0300

    Get All Regions

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalkDbContext dbContext;
+
+        public RegionsController(NZWalkDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            var Regions = dbContext.Regions.ToList();
+            return Ok(Regions);
+
+        }
+
+
+
+    }
+}
